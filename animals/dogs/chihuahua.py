484dea6e5e520da9f33b89201b8943e5b42428c6
Paul Romano <paul.k.romano@gmail.com>
2017-10-25 14:19:55 -0500

    Add a docstring for Chihuahua

--- animals/dogs/chihuahua.py
+++ animals/dogs/chihuahua.py
@@ -1,4 +1,13 @@
 from .dog import Dog
 
 class Chihuahua(Dog):
-    pass
+    """Smallest breed of dog, named after Chihuahua, Mexico
+
+    :param str name: Name of the Chihuahua
+    :param int age: Age of the Chihuahua
+
+    """
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
